070461a00d4f79265c16d45784ee20cbf6124d85
Jason Galvin <jasongalvin@gmail.com>
2012-12-20 20:07:18 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeclarativeAuthorizationRails3ExampleApp::Application.config.secret_token = 'a979c8a5a4661e0ae075d0cbeafabb8d972dcb5ba219ace00d6b227af46ad56e2e4764baa04f309e04e57845cae076fdbb867a2f5146324a348407b8b52548df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeclarativeAuthorizationRails3ExampleApp::Application.config.session_store :cookie_store, key: '_declarative_authorization_rails_3_example_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeclarativeAuthorizationRails3ExampleApp::Application.config.session_store :active_record_store
