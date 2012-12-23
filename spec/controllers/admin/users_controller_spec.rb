014a61b70622507d004673136d7e81cbe0f5adef
Jason Galvin <jasongalvin@gmail.com>
2012-12-22 23:47:50 -0800

    Restrict admin namespace to admin role

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -11,15 +11,38 @@ describe Admin::UsersController do
       end
     end
     
-    context "when user is logged in" do
+    context "when user does not have admin role" do
+      
       let(:user) { FactoryGirl.create(:user) }
+      
       before do
         sign_in user
+        role = FactoryGirl.create :role, :name => 'author'
+        FactoryGirl.create :assignment, :user => user, :role => role
       end
+      
+      it "allows the page to be viewed" do
+        get :index
+        response.should_not be_success
+      end
+      
+    end
+    
+    context "when user is admin" do
+      
+      let(:user) { FactoryGirl.create(:user) }
+      
+      before do
+        sign_in user
+        role = FactoryGirl.create :role, :name => 'admin'
+        FactoryGirl.create :assignment, :user => user, :role => role
+      end
+      
       it "allows the page to be viewed" do
         get :index
         response.should be_success
       end
+      
     end
     
   end
